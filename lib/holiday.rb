a7e3aba9758b62eaf3d130b8c885b18a4c546594
Emily Wagner <emilywagner598@gmail.com>
2019-01-20 02:07:35 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -58,7 +58,7 @@ def all_supplies_in_holidays(holiday_hash)
   # etc.
 
   holiday_hash.each do |season, holiday|
-    puts "#{season.capitalize}: "
+    puts "#{season.capitalize}:"
     #holiday.each do |
   end
 
