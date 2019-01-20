2925a4a41b4301f8e7433b715bbe942bcfe8b95b
Emily Wagner <emilywagner598@gmail.com>
2019-01-20 02:38:06 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -61,7 +61,7 @@ def all_supplies_in_holidays(holiday_hash)
     puts "#{season.capitalize}:"
     holiday.each do |holiday_name, supplies|
       string = holiday_name
-      string.str.gsub!(/_/, ' ')
+      string.gsub!(/_/, ' ')
       puts "  #{string.capitalize}: #{supplies.join(", ")}"
     end
   end
