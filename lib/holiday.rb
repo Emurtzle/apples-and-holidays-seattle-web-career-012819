446ccca46a96242b9170b91122749901823f82df
Emily Wagner <emilywagner598@gmail.com>
2019-01-20 02:58:45 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -71,5 +71,7 @@ def all_holidays_with_bbq(holiday_hash)
   # return an array of holiday names (as symbols) where supply lists
   # include the string "BBQ"
 
-  
+  holiday_hash.each do |holiday_name, supplies|
+    if (supplies.include("BBQ"))
+  end
 end
